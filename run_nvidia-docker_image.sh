28ef48a48c442fabe2df5db46183296ecd2a0b17
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2018-02-08 12:35:20 -0800

    fixed data volume to greg's path

--- run_nvidia-docker_image.sh
+++ run_nvidia-docker_image.sh
@@ -7,6 +7,7 @@ nvidia-docker run --rm -ti --ipc=host \
     -e "PASSWORD=jupyter1" \
     -p ${PORT}:9999 \
     -v /allen/aics/modeling/${USER}/projects:/root/projects \
+    -v /allen/aics/modeling/gregj/results:/root/data \
     -v /allen/aics:/root/aics \
     rorydm/pytorch_extras:${TAG} \
     bash -c "jupyter notebook --allow-root --NotebookApp.iopub_data_rate_limit=10000000000"
